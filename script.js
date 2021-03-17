fb1dd6f5caba2291517609147ab876c7162ae451
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-03-17 20:21:25 +0530

    second commit
    projects files commited | fixed scrolling

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ const mobileNavbar = document.querySelector('.mobile-navbar-container');
 openMenuButton.addEventListener('click', () => {
   showMainNavbar();
   mobileNavbar.classList.add('close');
+  const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
 });
 
 closeMenuButton.addEventListener('click', () => {
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
   closeMainNavbar();
   mobileNavbar.classList.remove('close');
+});
+
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
 });
 
 //functions
