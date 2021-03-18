c48e3be5f348f0cc44f8cfc0ab8d4528afeb1235
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-03-18 20:53:04 +0530

    third commit
    created services section

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // selectors
+const header = document.querySelector('.header');
+const projectsSlideshow = document.querySelector('.projects-slideshow');
 const mainNavbar = document.querySelector('.main-navbar');
 const openMenuButton = document.querySelector('[data-menu-button]');
 const closeMenuButton = document.querySelector('[data-close-menu-button]');
 const mobileNavbar = document.querySelector('.mobile-navbar-container');
 
 // Event listeners
+header.addEventListener('click', (e) => {
+  closeMainNavbar();
+  closeMobileNavbar();
+});
+
+projectsSlideshow.addEventListener('click', (e) => {
+  closeMainNavbar();
+  closeMobileNavbar();
+});
+
 openMenuButton.addEventListener('click', () => {
   showMainNavbar();
-  mobileNavbar.classList.add('close');
+  showMobileNavbar();
   const body = document.body;
   body.style.position = 'fixed';
   body.style.top = `-${scrollY}`;
@@ -20,7 +32,7 @@ closeMenuButton.addEventListener('click', () => {
   body.style.top = '';
   window.scrollTo(0, parseInt(scrollY || '0') * -1);
   closeMainNavbar();
-  mobileNavbar.classList.remove('close');
+  closeMobileNavbar();
 });
 
 window.addEventListener('scroll', () => {
@@ -31,6 +43,14 @@ window.addEventListener('scroll', () => {
 function showMainNavbar() {
   mainNavbar.classList.remove('close');
   mainNavbar.classList.add('open');
+}
+
+function showMobileNavbar() {
+  mobileNavbar.classList.add('close');
+}
+
+function closeMobileNavbar() {
+  mobileNavbar.classList.remove('close');
 }
 
 function closeMainNavbar() {
