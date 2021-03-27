1c619904a840f98b7e1cc22889fee3a4bb8e9dd7
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-03-27 23:14:17 +0530

    seventh-commit-2
    created a awesome autoplay carousel using flickity

--- script.js
+++ script.js
@@ -12,32 +12,3 @@ const menuButton = document.querySelector('.menu-button-black');
 menuButton.addEventListener('click', (e) => {
   mobileMenuContainer.classList.toggle('open');
 });
-
-// project gallery
-const slider = document.querySelector('.projects-wrapper');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', (_) => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', (_) => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 3;
-  const walk = (x - startX) * SCROLL_SPEED;
-  slider.scrollLeft = scrollLeft - walk;
-});
