da26fb00a0735ac00b3070ee524ef96ab17cf49d
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-03-28 22:38:46 +0530

    eigth commit
    created contact page and footer

---
+++ contact.js
@@ -0,0 +1,15 @@
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+const menuButton = document.querySelector('.menu-button-black');
+const profileImg = document.querySelector('.desktop-credential-profile-pic');
+const navName = document.querySelector('.desktop-credential-text');
+// Event listeners
+menuButton.addEventListener('click', (e) => {
+  mobileMenuContainer.classList.toggle('open');
+});
+
+profileImg.addEventListener('click', redirectToHome);
+navName.addEventListener('click', redirectToHome);
+
+function redirectToHome() {
+  location.href = '/index.html';
+}
--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 // selectors
-const header = document.querySelector('.header');
-const projectsSlideshow = document.querySelector('.projects-slideshow');
-const mainNavbar = document.querySelector('.main-navbar');
-const openMenuButton = document.querySelector('[data-menu-button]');
-const closeMenuButton = document.querySelector('[data-close-menu-button]');
-const mobileNavbar = document.querySelector('.mobile-navbar-container');
+
 const mobileMenuContainer = document.querySelector('.mobile-menu-container');
 const menuButton = document.querySelector('.menu-button-black');
 
