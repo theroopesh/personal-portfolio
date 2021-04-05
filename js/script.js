e3f8120eba89e8b72e3ed1e23825d43c655abc51
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-04-05 15:38:59 +0530

    v1.1
    added loading animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // selectors
+//loader selectors
+const loaderWrapper = document.querySelector(".loader-wrapper");
 
-const mobileMenuContainer = document.querySelector('.mobile-menu-container');
-const menuButton = document.querySelector('.menu-button-black');
+//Mobile menu selectors
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+const menuButton = document.querySelector(".menu-button-black");
 
 // Event listeners
-menuButton.addEventListener('click', (e) => {
-  mobileMenuContainer.classList.toggle('open');
+menuButton.addEventListener("click", (e) => {
+  mobileMenuContainer.classList.toggle("open");
+});
+
+window.addEventListener("load", (e) => {
+  loaderWrapper.style.display = "none";
 });
