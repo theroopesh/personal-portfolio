60f14abcf36ba7d8ba04cc9e0076dda192faeaaf
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-04-29 23:44:35 +0530

    v1.1
    almost finished

--- js/script.js
+++ js/script.js
@@ -1,16 +1,81 @@
 // selectors
-//loader selectors
-const loaderWrapper = document.querySelector(".loader-wrapper");
-
 //Mobile menu selectors
 const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 const menuButton = document.querySelector(".menu-button-black");
+const sectionOne = document.querySelector(".intro-section");
+const sectionFive = document.querySelector("#section-5");
+const header = document.querySelector(".header");
+const desktopNav = document.querySelector(".desktop-navbar");
+const navHeight = desktopNav.getBoundingClientRect().height;
+const banner = document.querySelector(".banner");
+const bannerHeight = banner.getBoundingClientRect().height;
+const navLinks = document.querySelector(".nav-links");
+const navLinksMobile = document.querySelector(".nav-links-mobile");
+const buttonPrimary = document.querySelector(".btn-primary");
+const buttonSecondary = document.querySelector("#btn-secondary");
+const buttonTertiary = document.querySelector(".btn-tertiary");
+const buttonHire = document.querySelector(".btn-hire");
 
 // Event listeners
 menuButton.addEventListener("click", (e) => {
   mobileMenuContainer.classList.toggle("open");
 });
 
-window.addEventListener("load", (e) => {
-  loaderWrapper.style.display = "none";
+// smooth scrolling desktop
+
+buttonPrimary.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionFive.scrollIntoView({ behavior: "smooth" });
+});
+
+buttonSecondary.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionFive.scrollIntoView({ behavior: "smooth" });
+});
+
+buttonTertiary.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionFive.scrollIntoView({ behavior: "smooth" });
+});
+
+buttonHire.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionFive.scrollIntoView({ behavior: "smooth" });
+});
+
+navLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Matching Strategy
+  const options = {
+    behavior: "smooth",
+  };
+
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    if (!id) {
+      location.href = "index.html";
+      return;
+    }
+    document.querySelector(id).scrollIntoView(options);
+  }
+});
+
+// smooth scrolling mobile
+navLinksMobile.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Matching Strategy
+  const options = {
+    behavior: "smooth",
+  };
+
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    if (!id) {
+      location.href = "index.html";
+      return;
+    }
+    document.querySelector(id).scrollIntoView(options);
+  }
 });
---
+++ js/thanks.js
@@ -0,0 +1,9 @@
+// selectors
+//Mobile menu selectors
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+const menuButton = document.querySelector(".menu-button-black");
+
+// Event listeners
+menuButton.addEventListener("click", (e) => {
+  mobileMenuContainer.classList.toggle("open");
+});
