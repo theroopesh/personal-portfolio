ef79b148d9203999c84abf637c265eef4da407c4
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-07-11 15:15:33 +0530

    v1.2
    big change UI

--- js/script.js
+++ js/script.js
@@ -7,14 +7,21 @@ const sectionFive = document.querySelector("#section-5");
 const header = document.querySelector(".header");
 const desktopNav = document.querySelector(".desktop-navbar");
 const navHeight = desktopNav.getBoundingClientRect().height;
-const banner = document.querySelector(".banner");
-const bannerHeight = banner.getBoundingClientRect().height;
+// const banner = document.querySelector(".banner");
+// const bannerHeight = banner.getBoundingClientRect().height;
 const navLinks = document.querySelector(".nav-links");
 const navLinksMobile = document.querySelector(".nav-links-mobile");
-const buttonPrimary = document.querySelector(".btn-primary");
-const buttonSecondary = document.querySelector("#btn-secondary");
-const buttonTertiary = document.querySelector(".btn-tertiary");
-const buttonHire = document.querySelector(".btn-hire");
+const nameInput = document.querySelector("#name");
+// const tipContainer = document.querySelector(".tip-container");
+// const tipCloseBtn = document.querySelector(".tip-cancel-button");
+
+// setTimeout(() => {
+//   tipContainer.classList.toggle("open");
+// }, 3000);
+
+// tipCloseBtn.addEventListener("click", (e) => {
+//   tipContainer.classList.toggle("open");
+// });
 
 // Event listeners
 menuButton.addEventListener("click", (e) => {
@@ -22,44 +29,44 @@ menuButton.addEventListener("click", (e) => {
 });
 
 // smooth scrolling desktop
-
-buttonPrimary.addEventListener("click", (e) => {
-  e.preventDefault();
-  sectionFive.scrollIntoView({ behavior: "smooth" });
-});
-
-buttonSecondary.addEventListener("click", (e) => {
-  e.preventDefault();
-  sectionFive.scrollIntoView({ behavior: "smooth" });
-});
-
-buttonTertiary.addEventListener("click", (e) => {
-  e.preventDefault();
-  sectionFive.scrollIntoView({ behavior: "smooth" });
-});
-
-buttonHire.addEventListener("click", (e) => {
-  e.preventDefault();
-  sectionFive.scrollIntoView({ behavior: "smooth" });
+// for multiple buttons to the section-5
+document.querySelectorAll(".scroll-to-section-5").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    sectionFive.scrollIntoView({ behavior: "smooth" });
+    focusNameInput();
+  });
 });
 
 navLinks.addEventListener("click", (e) => {
   e.preventDefault();
-
   // Matching Strategy
   const options = {
     behavior: "smooth",
   };
-
   if (e.target.classList.contains("nav-link")) {
     const id = e.target.getAttribute("href");
     if (!id) {
       location.href = "index.html";
-      return;
+    } else if (id === "#section-5") {
+      document.querySelector(id).scrollIntoView(options);
+      focusNameInput();
+    } else {
+      document.querySelector(id).scrollIntoView(options);
     }
-    document.querySelector(id).scrollIntoView(options);
   }
 });
+
+// helper functions
+function focusNameInput() {
+  setTimeout(() => {
+    nameInput.focus();
+  }, 1100);
+}
+
+function closeMenu() {
+  mobileMenuContainer.classList.toggle("open");
+}
 
 // smooth scrolling mobile
 navLinksMobile.addEventListener("click", (e) => {
@@ -74,8 +81,13 @@ navLinksMobile.addEventListener("click", (e) => {
     const id = e.target.getAttribute("href");
     if (!id) {
       location.href = "index.html";
-      return;
+    } else if (id === "#section-5") {
+      document.querySelector(id).scrollIntoView(options);
+      focusNameInput();
+      closeMenu();
+    } else {
+      document.querySelector(id).scrollIntoView(options);
+      closeMenu();
     }
-    document.querySelector(id).scrollIntoView(options);
   }
 });
--- js/thanks.js
+++
@@ -1,9 +0,0 @@
-// selectors
-//Mobile menu selectors
-const mobileMenuContainer = document.querySelector(".mobile-menu-container");
-const menuButton = document.querySelector(".menu-button-black");
-
-// Event listeners
-menuButton.addEventListener("click", (e) => {
-  mobileMenuContainer.classList.toggle("open");
-});
